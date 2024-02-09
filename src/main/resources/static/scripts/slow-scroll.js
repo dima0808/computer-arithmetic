b9f57ebc9856e5d3bd5ba871759a4b2a70f3f8b5
protein2005 <alexeysokole@gmail.com>
2024-02-10 00:28:50 +0200

    new buttons

---
+++ src/main/resources/static/scripts/slow-scroll.js
@@ -0,0 +1,16 @@
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+     change.target.classList.add('element-show');
+    }
+  });
+}
+
+let options = {
+  threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.element-animation');
+
+for (let elm of elements) {
+  observer.observe(elm);
+}
